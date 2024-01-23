f9a8e653dad2eac0ea536b529b7dbb06861b72af
ak010cse <ak010cse@gmail.com>
2024-01-24 01:10:22 +0530

    implement copy functionality

---
+++ app/src/main/java/com/arvind/qrcodescanner/CaptureActivityPortrait.kt
@@ -0,0 +1,6 @@
+package com.arvind.qrcodescanner
+
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CaptureActivityPortrait: CaptureActivity() {
+}
--- app/src/main/java/com/arvind/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/arvind/qrcodescanner/MainActivity.kt
@@ -1,11 +1,95 @@
 package com.arvind.qrcodescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.label
+import android.R.attr.text
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.arvind.qrcodescanner.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+
 
 class MainActivity : AppCompatActivity() {
+    val PERMISSION_REQUEST_CAMERA = 1
+    private lateinit var binding: ActivityMainBinding
+    var copiedText=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rlCopy.setOnClickListener {
+            val clipboard: ClipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("label", copiedText)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "Copied to Clipboard!!!", Toast.LENGTH_LONG).show()
+
+        }
+
+        binding.qrCodeScan.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
+                } else {
+                    initQRCodeScanner()
+                }
+            } else {
+                initQRCodeScanner()
+            }
+        }
+
     }
+
+    private fun initQRCodeScanner() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setOrientationLocked(true)
+        integrator.setCaptureActivity(CaptureActivityPortrait::class.java)
+        integrator.setPrompt("Scan a QR code")
+        integrator.initiateScan()
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initQRCodeScanner()
+            } else {
+                Toast.makeText(this, "Camera permission is required", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Scan cancelled", Toast.LENGTH_LONG).show()
+                binding.rlCopy.visibility = View.GONE
+                binding.llData.visibility = View.GONE
+                binding.scannedDataTv.text= ""
+            } else {
+                copiedText = result.contents
+                binding.scannedDataTv.text = result.contents
+                binding.rlCopy.visibility = View.VISIBLE
+                binding.llData.visibility = View.VISIBLE
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
 }
